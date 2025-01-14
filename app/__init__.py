227a2fe532cfec193f91e36bf4bf89ec1ed621dd
Jenani-N <jenani.nedumaran@gmail.com>
2025-01-14 14:50:13 -0500

    Added welcome message

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dhfbdbfjksd hfjksdkjdh'
 
+
+    @app.route('/')
+    def homepage():
+        return "Welcome!"
+
+
     return app
