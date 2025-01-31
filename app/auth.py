bdd903e93e8ef1a75371cede149afe39150921f6
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2025-01-30 19:41:38 -0500

    ..

--- app/auth.py
+++ app/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
-
+from flask import Blueprint, render_template, request, redirect, url_for, flash, send_from_directory
+import os
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "Login"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -12,5 +12,5 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
-    return "Sign Up"
+    return render_template("signup.html")
 
