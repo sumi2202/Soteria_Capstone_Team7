3801112d3b3ebbddd531934c363fdbd71a8fc91c
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2025-02-02 15:01:12 -0500

    Update auth.py

--- webapp/auth.py
+++ webapp/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, send_from_directory
-from user.models import User
+from .models import User
+from . import db
 import os
 auth = Blueprint('auth', __name__)
 
@@ -7,6 +8,15 @@ auth = Blueprint('auth', __name__)
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = db.users.find_one({"email": email, "password" : password})
+        if user:
+            flash('Log In Successful!', category= 'success')
+            return redirect(url_for('views.dashboard'))
+        else:
+
+
     return render_template("login.html")
 
 @auth.route('/logout')
