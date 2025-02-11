9978c00017d35986fb3f7c129ec25b9955df093b
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2025-02-11 14:28:29 -0500

    changes

--- webapp/views.py
+++ webapp/views.py
@@ -14,5 +14,13 @@ def sign_options():
 def dashboard():
     return render_template("dash_search.html")
 
+@views.route('/validation')
+def link_validation():
+    return render_template("link_validation.html")
+
+@views.route('/register-link')
+def link_registration():
+    return render_template("link_registration.html")
+
 
 
