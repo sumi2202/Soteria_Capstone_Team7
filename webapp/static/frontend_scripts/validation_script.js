28880c553474b556c01031a38afbff3beed48d60
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2025-02-13 17:24:25 -0500

    validation wip

--- webapp/static/frontend_scripts/validation_script.js
+++ webapp/static/frontend_scripts/validation_script.js
@@ -5,17 +5,20 @@ async function validateURL(){
     const response = await fetch('/validate', {
         method: 'POST',
         headers: {'Content-Type' : 'application/json'},
-        body: JSON.stringify({email,url})
+        body: JSON.stringify({url})
     });
 
     const result = await response.json();
     let message = '';
 
-    if (result.validURL === 1){
+    if(result.error){
+        message = "error: " + result.error;
+    }else if (result.validURL === 1){
         message = "Invalid or unreachable URL.";
     } else if (result.alreadyRegistered === 1){
         message = "already registered";
     } else {
         message = "URL available";
     }
+    document.getElementById('result').innerText = message;
 }
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash, send_from_directory, jsonify
-from backend_scripts.URLValidation import url_validation
+from flask import Blueprint, render_template, request, redirect, url_for, flash, send_from_directory, jsonify, session
+from .backend_scripts.URLValidation import url_validation
 import os
 
 views = Blueprint('views', __name__)
@@ -15,18 +15,22 @@ def sign_options():
 def dashboard():
     return render_template("dash_search.html")
 
-@views.route('/validation', methods=['GET', 'POST'])
+@views.route('/validation', methods=['GET','POST'])
 def link_validation():
-    data = request.json
-    email = data.get('email','')
-    url = data.get('url','')
+    if request.method == 'POST':
+        data = request.get_json()
+        url = data.get('url','')
 
-    validURL, alreadyRegistered = url_validation(email, url)
+        email = session.get('email')
+        if not email:
+            return jsonify({'error': 'User not logged in'}), 401
 
-    return jsonify({
+        validURL, alreadyRegistered = url_validation(email, url)
+
+        return jsonify({
         'validURL' : validURL,
         'alreadyRegistered' : alreadyRegistered
-    })
+     })
     return render_template("link_validation.html")
 
 @views.route('/register-link')
