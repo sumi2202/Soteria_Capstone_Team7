05bbe24bc6cbc3061c0cd238c3bab9cf6bcba4a6
Jenani-N <jenani.nedumaran@gmail.com>
2025-03-29 20:32:30 -0400

    GUI

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from webapp import create_app
 from flask import Flask, session
 from flask_session import Session
 
+# Create Flask app
 app = create_app()
 app.secret_key = "SOFECapstone2425"
 app.config["SESSION_PERMANENT"] = True
@@ -10,4 +11,4 @@ app.config["SESSION_FILE_DIR"] = "./flask_session"
 Session(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5001, debug=True, use_reloader=False)
